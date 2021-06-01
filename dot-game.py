21e987463431aade90d4096c6d19a98ca55a713d
Thelostium <thelostium@gmail.com>
2021-05-31 20:55:53 -0400

    Un pequeño lavado de cara y una funcionalidad

--- dot-game.py
+++ dot-game.py
@@ -1,9 +1,10 @@
-tablero = [["0"],[]]
-def juego():	
+def juego():
+	global board
+	global rangx
 	print('Hola, bienvenido a un microgame! \n \n')
 	input('presione enter para comenzar \n')
 	columns = input("Desea ver las instrucciones?")
-	columns.lower
+	columns.lower()
 	if columns.count("si") > 0 :
 		print("aqui estan las instrucciones: \n 1-Solo di 2 cordenadas,", end=" ")
 		print("la 1ª es de las columnas y la 2ª las filas,")
@@ -11,120 +12,87 @@ def juego():
 		print("3-tambien lo haran las de arriba, abajo izquierda y derecha")
 		print("4-si consigues que todas las '#' se vuelvan 'O' entonces ganas")
 		input("presiona enter cuando quieras comenzar")
-	print("vale,")
-	print("aqui esta el tablero:")
-	columns = 1
-	while columns <= 2:
-		if columns == 1:
-			print(" ", end=" ")
-			for lines in range(1,7):
-				print(lines, end=" ")
-				tablero[0].append(str(lines))
+	while True:
+		rangy = input("vale, cuantas filas quieres?: ")
+		rangx = input("ahora, cuantas columnas quieres?: ")
+		if rangy.isdigit() and rangx.isdigit() and int(rangy) >= 2 and int(rangy)<=500 and int(rangx)>=2 and int(rangx)<=500:
+			break
 		else:
-			counter = 2
-			print("")
-			for lines in "12345":
-				print(lines, end=" ")
-				tablero.append([lines])
-				for buttons in range(1,7):
-					print("O", end=" ")
-					tablero[counter].append('O')
-				print("")
-				counter += 1
-		columns += 1
-	tablero.pop(1)
-	return tablero
+			print("sorry el rango es de (filas y columnas) mayores o iguales a 2 y menores o iguales a 500, ademas que tienen que ser numeros enteros.")
+			print("intentemoslo otra vez\n")
+	print("\nvale,\n\naqui esta el tablero:\n")
+	board = [["#" for j in range(0,int(rangx))] for i in range(0,int(rangy))]
+	print(" ".join([str(i) for i in range(0,int(rangx)+1)]))
+	[print(b+" "+" ".join(i)) for i,b in zip(board,"ABCDEFGH")]
+	print("")
+
 while True:
 	juego()
-	win = False
-	while win == False:
+	while True:
 		try:
-			control1 = int(input("seleccione aqui la fila de 1 a 5: "))
-			control2 = int(input("seleccione aqui la columna de 1 a 6: "))
-			if control1<=10 and control1>=1 and control2<=6 and control2>=1:
-				if tablero[control1][control2] == "O":
-
-					tablero[control1][control2] = 'X'
-				else:
-					tablero[control1][control2] = "O"
-				try:
-					if tablero[control1][control2 - 1] == "O":
-						if str(tablero[control1][control2 - 1]) == 'X' or (tablero[control1][control2 - 1]) == "O":
-							tablero[control1][control2 - 1] = 'X'
-						else:
-							pass
-					else:
-						if str(tablero[control1][control2 - 1]) == 'X' or (tablero[control1][control2 - 1]) == "O":
-							tablero[control1][control2 - 1] = "O"
-						else:
-							pass
-				except IndexError:
-					pass
-				try:
-					if tablero[control1 + 1][control2] == "O":
-						tablero[control1 + 1][control2] = 'X'
-					else:
-						tablero[control1 + 1][control2] = "O"
-				except IndexError:
-					pass
-				try:
-					if tablero[control1][control2 + 1] == "O":
-						tablero[control1][control2 + 1] = 'X'
-					else:
-						tablero[control1][control2 + 1] = "O"
-				except IndexError:
-					pass
-				try:	
-					if tablero[control1 - 1][control2] == "O":
-						if str(tablero[control1 - 1][control2]) == 'X' or (tablero[control1 - 1][control2]) == "O":
-							tablero[control1 - 1][control2] = 'X'
-					else:
-						if str(tablero[control1 - 1][control2]) == 'X' or (tablero[control1 - 1][control2]) == "O":
-							tablero[control1 - 1][control2] = "O"
-				except IndexError:
-					pass
-			else:
-				print("tecla incorrecta :(")
-				pass
-			for i in range(len(tablero)):
-				print(tablero[i])
+			y = int(input("seleccione aqui la fila de 1 a 8: "))
+			x = int(input("seleccione aqui la columna de 1 a 7: "))
 		except ValueError:
-			print("Solo numeros naturales :) \n")
+			print("Valor no correcto, solo numeros enteros :) \n")
 			continue
-		except IndexError:
-			print("Solo numeros en rango ya dicho :)")
-			continue
-		victoria = []
-		for i in range(len(tablero)- 1):
+		if y<=8 and y>=1 and x<=7 and x>=1:
+			x-=1
+			y-=1
+			if board[y][x] == "O":
+				board[y][x] = "#"
+			else:
+				board[y][x] = "O"
 			try:
-				victoria.append(tablero[i+1].count("X"))
-				if int(victoria[i]) == 0:
-					victoria.pop(i)
-					victoria.append(True)
+				if x-1 == -1:
+					pass
+				elif board[y][x - 1] == "O":
+					board[y][x - 1] = "#"
 				else:
-					victoria.pop(i)
-					victoria.append(False)
+					board[y][x - 1] = "O"
 			except IndexError:
-				victoria.append(tablero[i].count("X"))
-				if int(victoria[i]) == 0:
-					victoria.pop(i)
-					victoria.append(True)
+				pass
+			try:
+				if board[y + 1][x] == "O":
+					board[y + 1][x] = "#"
 				else:
-					victoria.pop(i)
-					victoria.append(False)
-		print(victoria)
-		if int(victoria.count(True)) == int(len(tablero)-1):
-			print(victoria.count(True))
-			win = True
-			continue
+					board[y + 1][x] = "O"
+			except IndexError:
+				pass
+			try:
+				if board[y][x + 1] == "O":
+					board[y][x + 1] = "#"
+				else:
+					board[y][x + 1] = "O"
+			except IndexError:
+				pass
+			try:	
+				if y-1 == -1:
+					pass
+				elif board[y - 1][x] == "O":
+					board[y - 1][x] = "#"
+				else:
+					board[y - 1][x] = "O"
+			except IndexError:
+				pass
 		else:
+			print("Tienes que introducir un numero en el rango permitido, ponlo otra vez:\n")
 			continue
+		if all([i.count("#") == 0 for i in board]):
+			print(" ".join([str(i) for i in range(0,int(rangx)+1)]))
+			[print(b+" "+" ".join(i)) for i,b in zip(board,"ABCDEFGH")]
+			print("")
+			break
+		else:
+			print(" ".join([str(i) for i in range(0,int(rangx)+1)]))
+			[print(b+" "+" ".join(i)) for i,b in zip(board,"ABCDEFGH")]
+			print("")
+			continue
+	print("")
 	print("Ganaste!".center(30))
 	sel = input("Quieres jugar otra vez?".center(30))
-	sel.lower
+	sel.lower()
 	if sel.count("si") == 1:
 		continue
 	else:
 		break
-
 				
