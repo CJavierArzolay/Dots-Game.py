fbf235434081eb3cdcf51a91283327568cda4a9c
Thelostium <thelostium@gmail.com>
2021-06-03 19:04:41 -0400

    Latest design mistakes fixed

---
+++ d
@@ -0,0 +1,30 @@
+[33mcommit 2ce92275de54fb2fbe0872eb6c730a34298db67f[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31morigin/master[m[33m)[m
+Merge: 86162f0 a25024c
+Author: Thelostium <thelostium@gmail.com>
+Date:   Tue Jun 1 14:11:08 2021 -0400
+
+    Merge branch 'master' of https://github.com/Thelostium/Dots-Game.py
+
+[33mcommit 86162f0b085b67a723c662d80f4b96befe9f83de[m
+Author: Thelostium <thelostium@gmail.com>
+Date:   Mon May 31 21:02:10 2021 -0400
+
+    Maximo de puntos repensado
+
+[33mcommit 21e987463431aade90d4096c6d19a98ca55a713d[m
+Author: Thelostium <thelostium@gmail.com>
+Date:   Mon May 31 20:55:53 2021 -0400
+
+    Un pequeño lavado de cara y una funcionalidad
+
+[33mcommit a25024c8f0e6eea3b8a32c275925127d1722d115[m
+Author: Thelostium0.1a <thelostium@gmail.com>
+Date:   Mon May 31 17:19:25 2021 -0400
+
+    A simply readme
+
+[33mcommit f1d423f544083052680e38361f01f92d050b0ed3[m
+Author: Thelostium <thelostium@gmail.com>
+Date:   Sat May 29 18:45:55 2021 -0400
+
+    El juego que hice en mas o menos hace 3 meses
--- dot-game.py
+++ dot-game.py
@@ -1,41 +1,62 @@
 def juego():
+	global rangy
 	global board
 	global rangx
+
+	#Presentation	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	
 	print('Hola, bienvenido a un microgame! \n \n')
 	input('presione enter para comenzar \n')
-	columns = input("Desea ver las instrucciones?")
-	columns.lower()
-	if columns.count("si") > 0 :
-		print("aqui estan las instrucciones: \n 1-Solo di 2 cordenadas,", end=" ")
-		print("la 1ª es de las columnas y la 2ª las filas,")
+
+	#Instructive	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-
+	rangy = input("Desea ver las instrucciones?").lower()
+	if rangy.count("si")>0 or rangy.count("s")>0 :
+		print("aqui están: \n\n1-Solo di 2 numeros,", end=" ")
+		print("el 1º es de las filas (de arriba a abajo) \ny el 2º las columnas (izquierda a derecha),")
 		print("2-al seleccionar uno de estos '#' se volvera una 'O' y viseversa")
 		print("3-tambien lo haran las de arriba, abajo izquierda y derecha")
-		print("4-si consigues que todas las '#' se vuelvan 'O' entonces ganas")
-		input("presiona enter cuando quieras comenzar")
+		print("4-si consigues que todas las '#' se vuelvan 'O' entonces ganas. Facilito?")
+		input("presiona enter para continuar")
+
+	print("\nVale...\n")
+
+	#Board sizing	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-
 	while True:
-		rangy = input("vale, cuantas filas quieres?: ")
-		rangx = input("ahora, cuantas columnas quieres?: ")
-		if rangy.isdigit() and rangx.isdigit() and int(rangy) >= 2 and int(rangy)<=100 and int(rangx)>=2 and int(rangx)<=100:
+		rangy = input("cuantas filas quieres?: ")
+		if 2 <= int(rangy) <= 100 and rangy.isdigit():
 			break
-		else:
-			print("sorry el rango es de (filas y columnas) mayores o iguales a 2 y menores o iguales a 100, ademas que tienen que ser numeros enteros.")
-			print("intentemoslo otra vez\n")
+		print(f"\nEl valor {rangy} de rangy es {'menor que 2' if int(rangy) < 2 else 'mayor que 100'}" if rangy.isdigit() else f"\nEl valor {rangy} de rangy no es un numero entero")
+		print("intentemoslo otra vez:\n")
+	print("\nBien, ahora...\n")
+
+	while True:
+		rangx = input("cuantas columnas quieres?: ")
+		if 2 <= int(rangx) <= 100 and rangy.isdigit():
+			break	
+		print(f"\nEl valor {rangx} de rangx es {'menor que 2' if int(rangx) < 2 else 'mayor que 100'}" if rangx.isdigit() else f"\nEl valor {rangx} de rangy no es un numero entero")
+		print("intentemoslo otra vez:\n")
+		continue
+		
+	#Board creation		-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-
 	print("\nvale,\n\naqui esta el tablero:\n")
 	board = [["#" for j in range(0,int(rangx))] for i in range(0,int(rangy))]
 	print(" ".join([str(i) for i in range(0,int(rangx)+1)]))
-	[print(b+" "+" ".join(i)) for i,b in zip(board,"ABCDEFGH")]
+	[print(str(b)+" "+" ".join(i)) for i,b in zip(board,range(1,int(rangy)+1))]
 	print("")
 
 while True:
 	juego()
 	while True:
+
+		#Control	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-
 		try:
-			y = int(input("seleccione aqui la fila de 1 a 8: "))
-			x = int(input("seleccione aqui la columna de 1 a 7: "))
+			y = int(input(f"seleccione aqui la fila de 1 a {rangy}: "))
+			x = int(input(f"seleccione aqui la columna de 1 a {rangx}: "))
 		except ValueError:
-			print("Valor no correcto, solo numeros enteros :) \n")
+			print(f"El numero{y if x.isdigit() else x} no es un numero entero :) \n")
 			continue
-		if y<=8 and y>=1 and x<=7 and x>=1:
+
+		#Mechanic	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-
+		if 1 <= y <=int(rangy) and 1 <= x <= int(rangx) :
 			x-=1
 			y-=1
 			if board[y][x] == "O":
@@ -79,20 +100,21 @@ while True:
 			continue
 		if all([i.count("#") == 0 for i in board]):
 			print(" ".join([str(i) for i in range(0,int(rangx)+1)]))
-			[print(b+" "+" ".join(i)) for i,b in zip(board,"ABCDEFGH")]
+			[print(str(b)+" "+" ".join(i)) for i,b in zip(board,range(1,int(rangy)+1))]
 			print("")
 			break
 		else:
+			print("")
 			print(" ".join([str(i) for i in range(0,int(rangx)+1)]))
-			[print(b+" "+" ".join(i)) for i,b in zip(board,"ABCDEFGH")]
+			[print(str(b)+" "+" ".join(i)) for i,b in zip(board,range(1,int(rangy)+1))]
 			print("")
 			continue
-	print("")
-	print("Ganaste!".center(30))
+
+	#Victory Screen		-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-
+	print("\n"+"Ganaste!".center(30))
 	sel = input("Quieres jugar otra vez?".center(30))
 	sel.lower()
 	if sel.count("si") == 1:
 		continue
 	else:
 		break
-				
